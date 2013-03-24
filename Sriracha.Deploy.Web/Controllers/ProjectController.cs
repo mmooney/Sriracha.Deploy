08223f5bda662f35347e0235aa3185bcaf982479
Mike Mooney <mmooney@mmdbsolutions.com>
2013-03-24 15:09:20 -0400

    Started implementing web UI

---
+++ Sriracha.Deploy.Web/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sriracha.Deploy.Data.Dto;
+
+namespace Sriracha.Deploy.Web.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        // GET api/project
+        public IEnumerable<DeployProject> Get()
+        {
+            return new DeployProject[]
+			{
+				new DeployProject { Id="1", ProjectName="test project 1" },
+				new DeployProject { Id="2", ProjectName="test project 2" }
+			};
+        }
+
+        // GET api/project/5
+        public DeployProject Get(int id)
+        {
+			return new DeployProject { Id=id.ToString(), ProjectName="test project " + id.ToString() };
+        }
+
+        // POST api/project
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/project/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/project/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Sriracha.Deploy.Web/Scripts/Sriracha/index.js
@@ -0,0 +1,16 @@
+﻿Sriracha = {
+	init: function () {
+		Sriracha.projects = new Sriracha.ProjectCollection();
+
+		var projectListView = new Sriracha.ProjectListView({ collection: Sriracha.projects });
+		projectListView.render();
+
+		$(".projectList").append(projectListView.el);
+		Sriracha.projects.fetch();
+	},
+	start: function () {
+		Sriracha.init();
+		//also start router
+	}
+}
+
