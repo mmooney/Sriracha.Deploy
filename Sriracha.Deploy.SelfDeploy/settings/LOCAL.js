96703f60ab7813ef312b7f837bf3e0eac15273e9
Mike Mooney <mmooney@mmdbsolutions.com>
2013-07-05 17:22:03 -0400

    website deployment

---
+++ Sriracha.Deploy.SelfDeploy/settings/LOCAL.js
@@ -0,0 +1,9 @@
+﻿{
+	SourceWebsitePath: ".",
+	TargetWebsitePath: "C:\\test\\SrirachaDeployTest",
+	WebUserName: "NT AUTHORITY\\NETWORK SERVICE",
+	VirtualDirectorySite: "Sriracha",
+	VirtualDirectoryName: "",
+	RavenDBConnectionString: "Url=http://localhost:8080;Database=SrirachaDeployTest",
+	ApplicationPoolName: "SrirachaAppPool"
+}
