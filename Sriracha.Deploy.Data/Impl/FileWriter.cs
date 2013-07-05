5b8f6c08024b851cd0bad7fe8f986d69c3dfd529
Mike Mooney <mmooney@mmdbsolutions.com>
2013-07-05 11:25:54 -0400

    validation UI for environment settings

---
+++ Sriracha.Deploy.Data/Impl/FileWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sriracha.Deploy.Data.Impl
+{
+	public class FileWriter : IFileWriter
+	{
+		public void WriteText(string fileName, string text, bool append)
+		{
+			if(append)
+			{
+				File.AppendAllText(fileName, text);
+			}
+			else 
+			{
+				File.WriteAllText(fileName, text);
+			}
+		}
+	}
+}
