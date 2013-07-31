0a8696ca8684d8ba1ccaeb937c161c0f61b1aa1f
Mike Mooney <mmooney@mmdbsolutions.com>
2013-07-31 09:08:23 -0400

    Build/deploy

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+if '%1' == '/?' goto usage
+if '%1' == '-?' goto usage
+if '%1' == '?' goto usage
+if '%1' == '/help' goto usage
+
+SET DIR=%cd%
+SET BUILD_DIR=%~d0%~p0%
+SET NANT="%BUILD_DIR%ThirdParty\nant-0.92\NAnt.exe"
+
+%NANT% -logger:NAnt.Core.DefaultLogger /f:%BUILD_DIR%Sriracha.Deploy.build BuildSolution
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
+goto finish
+
+:usage
+echo.
+echo Usage: build.bat
+echo.
+goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
+
+:finish
