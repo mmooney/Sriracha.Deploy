bdb6acd9c006be04b6796d654f413518066144cc
Mike Mooney <mmooney@mmdbsolutions.com>
2013-08-02 10:00:47 -0400

    Command line fixes

--- Sriracha.Deploy.Data/Impl/ProcessRunner.cs
+++ Sriracha.Deploy.Data/Impl/ProcessRunner.cs
@@ -12,19 +12,35 @@ namespace Sriracha.Deploy.Data.Impl
 		public int Run(string executablePath, string executableParameters, TextWriter standardOutputWriter, TextWriter errorOutputWriter)
 		{
 			var psi = new ProcessStartInfo(executablePath, executableParameters);
+			psi.UseShellExecute = false;
 			psi.RedirectStandardError = true;
 			psi.RedirectStandardOutput = true;
-			psi.UseShellExecute = false;
-			var p = Process.Start(psi);
-			string line;
-			while ((line = p.StandardOutput.ReadLine()) != null)
+			psi.CreateNoWindow = true;
+			var p = new Process()
 			{
-				standardOutputWriter.WriteLine(line);
-			}
-			while ((line = p.StandardError.ReadLine()) != null)
+				StartInfo = psi
+			};
+			p.OutputDataReceived += (sender, args) =>
 			{
-				errorOutputWriter.WriteLine(line);
-			}
+				standardOutputWriter.WriteLine(args.Data);
+			};
+			p.ErrorDataReceived += (sender, args) =>
+			{
+				errorOutputWriter.WriteLine(args.Data);
+			};
+			p.Start();
+			p.BeginOutputReadLine();
+			p.BeginErrorReadLine();
+			//var p = Process.Start(psi);
+			//string line;
+			//while ((line = p.StandardOutput.ReadLine()) != null)
+			//{
+			//	standardOutputWriter.WriteLine(line);
+			//}
+			//while ((line = p.StandardError.ReadLine()) != null)
+			//{
+			//	errorOutputWriter.WriteLine(line);
+			//}
 			p.WaitForExit();
 			return p.ExitCode;
 		}
