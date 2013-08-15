f3dfb957830b6d69a09dc46680866edbdbc1e019
Mike Mooney <mmooney@mmdbsolutions.com>
2013-08-15 12:35:31 -0400

    RavenDB initialization error logging

--- Sriracha.Deploy.RavenDB/RavenHelper.cs
+++ Sriracha.Deploy.RavenDB/RavenHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using Raven.Client;
@@ -12,20 +13,35 @@ namespace Sriracha.Deploy.RavenDB
 	{
 		public static IDocumentStore CreateDocumentStore()
 		{
-			var documentStore = new DocumentStore()
+			try 
 			{
-				ConnectionStringName = "RavenDB",
-				Conventions = new DocumentConvention
+				var documentStore = new DocumentStore()
 				{
-					DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites,
-					ShouldCacheRequest = url => false 
+					ConnectionStringName = "RavenDB",
+					Conventions = new DocumentConvention
+					{
+						DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites,
+						ShouldCacheRequest = url => false 
+					}
+				};
+				documentStore.Initialize();
+				documentStore.DisableAggressiveCaching();
+				documentStore.DatabaseCommands.DisableAllCaching();
+				IndexCreation.CreateIndexes(typeof(RavenHelper).Assembly, documentStore);
+				return documentStore;
+			}
+			catch(Exception err)
+			{
+				var connStrObject = ConfigurationManager.ConnectionStrings["RavenDB"];
+				if(connStrObject == null || string.IsNullOrWhiteSpace(connStrObject.ConnectionString))
+				{
+					throw new Exception("Error initializing RavenDB DocumentStore, ConnectionString RavenDB is missing", err);
 				}
-			};
-			documentStore.Initialize();
-			documentStore.DisableAggressiveCaching();
-			documentStore.DatabaseCommands.DisableAllCaching();
-			IndexCreation.CreateIndexes(typeof(RavenHelper).Assembly, documentStore);
-			return documentStore;
+				else 
+				{
+					throw new Exception(string.Format("Error initializing RavenDB DocumentStore with ConnectionString \"{0}\"", connStrObject.ConnectionString), err);					
+				}
+			}
 		}
 	}
 }
