064682388a0377ca41b5517bf3039f260ee043d2
Mike Mooney <mmooney@mmdbsolutions.com>
2013-08-20 16:17:21 -0400

    Added testing UI for viewing file attachments

--- Sriracha.Deploy.Web/Controllers/TestingController.cs
+++ Sriracha.Deploy.Web/Controllers/TestingController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Sriracha.Deploy.Data;
+using Sriracha.Deploy.Data.Repository;
 
 namespace Sriracha.Deploy.Web.Controllers
 {
     public class TestingController : Controller
     {
         private readonly IDataGenerator _dataGenerator;
+		private readonly IFileRepository _fileRepository;
 
-		public TestingController(IDataGenerator dataGenerator)
+		public TestingController(IDataGenerator dataGenerator, IFileRepository fileRepository)
 		{
 			_dataGenerator = DIHelper.VerifyParameter(dataGenerator);
+			_fileRepository = DIHelper.VerifyParameter(fileRepository);
 		}
         public ActionResult Index()
         {
@@ -24,6 +27,13 @@ namespace Sriracha.Deploy.Web.Controllers
 		{
 			_dataGenerator.CreateSampleData();
 			return Content("Done");
+		}
+
+		public ActionResult File(string deployFileId, string contentType="application/octet-stream")
+		{
+			var fileObject = _fileRepository.GetFile(deployFileId);
+			var data = _fileRepository.GetFileData(deployFileId);
+			return File(data, contentType, fileObject.FileName);	
 		}
     }
 }
