245c37192dcf00019df1d19da8fd675f454254fc
Mike Mooney <mmooney@mmdbsolutions.com>
2013-10-23 09:28:58 -0400

    Fixes for build list on deploy request page

--- Sriracha.Deploy.Web/Scripts/Sriracha/ngSriracha.js
+++ Sriracha.Deploy.Web/Scripts/Sriracha/ngSriracha.js
@@ -146,62 +146,6 @@ ngSriracha.directive("taskConfig", function () {
 	}
 });
 
-ngSriracha.directive("selectEnvironmentMachines",
-		['SrirachaResource', 'ErrorReporter', 'SrirachaNavigator',
-		function (SrirachaResource, ErrorReporter, SrirachaNavigator) {
-	return {
-		restrict: "E",
-		templateUrl: "templates/directives/selectEnvironmentMachines.html",
-		scope: {
-			buildid: '@',
-			environmentid: '@',
-			selection: '='
-		},
-		link: function postLink(scope, element, attrs) {
-			scope.navigator = SrirachaNavigator;
-			scope.errorReporter = ErrorReporter;
-			scope.$watch("buildid + environmentid", function () {
-				if (!scope.buildid || !scope.environmentid) {
-					scope.build = null;
-					scope.environment = null;
-					scope.environmentSelector = null;
-				}
-				else {
-					scope.build = SrirachaResource.build.get({ id: scope.buildid },
-						function () {
-							scope.project = SrirachaResource.project.get({ id: scope.build.projectId },
-								function () {
-									scope.environmentSelector = new EnvironmentSelector(scope.build, scope.project, scope.environmentid, SrirachaResource, ErrorReporter,
-										function () {
-											if (scope.environmentSelector.environmentComponent) {
-												scope.selection.machineList = scope.environmentSelector.environmentComponent.machineList;
-												if (scope.selection.preselectedMachineIds) {
-													_.each(scope.selection.preselectedMachineIds, function (id) {
-														var machine = _.findWhere(scope.selection.machineList, { id: id });
-														if (machine) {
-															machine.selected = true;
-														}
-													});
-												}
-											}
-											if (!scope.selection.machineList || !scope.selection.machineList.length) {
-												scope.selection.machineList = [];
-											}
-										});
-								},
-								function (err) {
-									ErrorReporter.handleResourceError(error);
-								});
-						},
-						function(err) {
-							ErrorReporter.handleResourceError(err);
-						}
-					);
-				}
-			});
-		}
-	}
-}]);
 
 ngSriracha.directive("projectList", function () {
 	return {
