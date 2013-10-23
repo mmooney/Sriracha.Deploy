8aadb10c6c41edb0fac58bec0ea329ef0ca39d71
Mike Mooney <mmooney@mmdbsolutions.com>
2013-10-23 09:08:21 -0400

    Adding missing files

---
+++ Sriracha.Deploy.Web/Services/Build/BuildRequest.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.ServiceHost;
+using Sriracha.Deploy.Data.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sriracha.Deploy.Web.Services.Build
+{
+	[Route("/build")]
+	[Route("/build/{id*}")]
+	public class BuildRequest : RequestBase<DeployBuild>
+	{
+		public string Id { get; set; }
+		public string ProjectId { get; set; }
+		public string ProjectBranchId { get; set; }
+		public string ProjectComponentId { get; set; }
+		public string FileId { get; set; }
+		public string Version { get; set; }
+	}
+}
