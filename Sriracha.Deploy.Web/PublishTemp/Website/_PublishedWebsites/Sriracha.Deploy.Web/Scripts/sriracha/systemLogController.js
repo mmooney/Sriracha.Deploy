fa6bb0ff51893ccb0c163c5dfd1b2b3fd693ef90
Mike Mooney <mmooney@mmdbsolutions.com>
2013-10-29 20:17:34 -0400

    Removed temp files

--- Sriracha.Deploy.Web/PublishTemp/Website/_PublishedWebsites/Sriracha.Deploy.Web/Scripts/sriracha/systemLogController.js
+++
@@ -1,5 +0,0 @@
-﻿ngSriracha.controller("SystemLogController", function ($scope, $routeParams, SrirachaResource, ErrorReporter) {
-	$scope.systemLog = SrirachaResource.systemLog.get({  }, function () {
-		console.log($scope.systemLog);
-	}, ErrorReporter.handleResourceError);
-});
