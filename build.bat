66d2bf9b34418b6dd8213d1a53f111acf42978b5
Mike Mooney <mmooney@mmdbsolutions.com>
2014-04-23 09:30:33 -0400

    Build fixes

--- build.bat
+++ build.bat
@@ -8,6 +8,7 @@ if '%1' == '/help' goto usage
 SET DIR=%cd%
 SET BUILD_DIR=%~d0%~p0%
 SET NANT="%BUILD_DIR%ThirdParty\nant-0.92\NAnt.exe"
+SET BUILD_NUMBER="0.0.0.0""
 
 %NANT% -logger:NAnt.Core.DefaultLogger /f:%BUILD_DIR%Sriracha.Deploy.build BuildSolution
 
